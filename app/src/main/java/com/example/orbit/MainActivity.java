584139baad562bf5a2bad28b26191e36171dc59a
Nishita-23 <nishitagudipati@gmail.com>
2023-09-19 19:03:19 -0500

    planner

--- app/src/main/java/com/example/orbit/MainActivity.java
+++ app/src/main/java/com/example/orbit/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.orbit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the button by its ID
+        @SuppressLint("WrongViewCast") Button buttonGoToDestination = findViewById(R.id.imageButton4);
+
+        // Set an OnClickListener for the button
+        buttonGoToDestination.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+// Define what happens when the button is clicked
+                Intent intent = new Intent(MainActivity.this, homepage.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
