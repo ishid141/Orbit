a03da4a6337befc742e8c76118fc00bcd7cae9b2
Nishita-23 <nishitagudipati@gmail.com>
2023-10-14 13:07:30 -0500

    timerpage

--- app/src/main/java/com/example/orbit/timerpage.java
+++ app/src/main/java/com/example/orbit/timerpage.java
@@ -1,83 +1,62 @@
 package com.example.orbit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-//This is defining the buttons
 public class timerpage extends AppCompatActivity {
-    private TextView textView6;
-    private Button button4, button5, button6;
-    private Handler handler;
-    private long startTimeMillis;
-    private boolean isTimerRunning;
+    private EditText timeEditText;
+    private Button button4;
+    private TextView countdownTextView;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timerpage);
 
-        //This is starting the actual code
-        textView6 = findViewById(R.id.textView6);
+        timeEditText = findViewById(R.id.timeEditText);
         button4 = findViewById(R.id.button4);
-        button5 = findViewById(R.id.button5);
-        button6 = findViewById(R.id.button6);
+        countdownTextView = findViewById(R.id.countdownTextView);
 
-        handler = new Handler();
 
-        button5.setOnClickListener(v -> button5());
-        button4.setOnClickListener(v -> button4());
-        button6.setOnClickListener(v -> button6());
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
     }
-    private void button5() {
-        if (!isTimerRunning) {
-            startTimeMillis = System.currentTimeMillis();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
-                    updateTimer(elapsedTimeMillis);
-                    handler.postDelayed(this, 1000); // Update every 1 second
-                }
-            }, 1000); // Initial delay of 1 second
 
-            isTimerRunning = true;
-            button5.setEnabled(false);
-            button4.setEnabled(true);
-            button6.setEnabled(true);
+    private void startTimer() {
+        String timeText = timeEditText.getText().toString();
+        if (!timeText.isEmpty()) {
+            long timeInMillis = Long.parseLong(timeText) * 1000; // Convert seconds to milliseconds
+            countDownTimer = new CountDownTimer(timeInMillis, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeLeftInMillis = millisUntilFinished;
+                    updateCountdownText();
+                }
+
+                @Override
+                public void onFinish() {
+                    countdownTextView.setText("Countdown: 0");
+                }
+            }.start();
         }
     }
 
-    private void button4() {
-        isTimerRunning = false;
-        button5.setEnabled(true);
-        button4.setEnabled(false);
-        button6.setEnabled(true);
+    private void updateCountdownText() {
+        long seconds = timeLeftInMillis / 1000;
+        countdownTextView.setText("Countdown: " + seconds);
     }
-
-    private void button6() {
-        isTimerRunning = false;
-        updateTimer(0);
-        button5.setEnabled(true);
-        button4.setEnabled(false);
-        button6.setEnabled(false);
-    }
-
-    private void updateTimer(long elapsedTimeMillis) {
-        int seconds = (int) (elapsedTimeMillis / 1000);
-        int minutes = seconds / 60;
-        int hours = minutes / 60;
-
-        seconds %= 60;
-        minutes %= 60;
-
-        String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        textView6.setText(timeFormatted);
-
-    }
-
 }
+
