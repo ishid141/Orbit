c46f16994462a349df04b5db4bbb9709de75636d
ishid141 <ishid141@gmail.com>
2023-10-16 18:46:04 -0500

    resource

--- app/src/main/java/com/example/orbit/resourcepage.java
+++ app/src/main/java/com/example/orbit/resourcepage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.net.Uri;
 
 public class resourcepage extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class resourcepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resourcepage);
+
+        TextView textView = findViewById(R.id.textView5);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the action to be taken when the link is clicked (e.g., open a website)
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCEWpbFLzoYGPfuWUMFPSaoA"));
+                startActivity(intent);
+
+
+
+            }
+        });
     }
 }
