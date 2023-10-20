9c7c038d2015a4792cc379c5d84e455e5ddac482
Nishita-23 <nishitagudipati@gmail.com>
2023-10-20 18:03:46 -0500

    planner

--- app/src/main/java/com/example/orbit/plannerpage.java
+++ app/src/main/java/com/example/orbit/plannerpage.java
@@ -1,15 +1,29 @@
 package com.example.orbit;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class plannerpage extends AppCompatActivity {
+    private Button addTodoButton;
+    private LinearLayout todoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plannerpage);
+
+        addTodoButton = findViewById(R.id.addTodoButton);
+        todoList = findViewById(R.id.todoList);
+    }
+
+    public void addTodoItem(View view) {
+        TextView newTodo = new TextView(this);
+        newTodo.setText("New To-Do Item");
+        todoList.addView(newTodo);
+
     }
 }
