c9640271c99560da29ab94e26b54304c9adeb462
ishid141 <ishid141@gmail.com>
2023-10-20 18:26:09 -0500

    plannerfix

--- app/src/main/java/com/example/orbit/plannerpage.java
+++ app/src/main/java/com/example/orbit/plannerpage.java
@@ -3,6 +3,7 @@ package com.example.orbit;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class plannerpage extends AppCompatActivity {
     private Button addTodoButton;
     private LinearLayout todoList;
+    private EditText todoInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,25 @@ public class plannerpage extends AppCompatActivity {
         setContentView(R.layout.activity_plannerpage);
 
         addTodoButton = findViewById(R.id.addTodoButton);
+        todoInput = findViewById(R.id.todoInput);
         todoList = findViewById(R.id.todoList);
+
     }
 
     public void addTodoItem(View view) {
-        TextView newTodo = new TextView(this);
-        newTodo.setText("New To-Do Item");
-        todoList.addView(newTodo);
+
+        String newTodoText = todoInput.getText().toString();
+
+        if (!newTodoText.isEmpty()){
+
+            TextView newTodo = new TextView(this);
+            newTodo.setText(newTodoText);
+
+            todoList.addView(newTodo);
+
+            todoInput.setText("");
+        }
+
 
     }
 }
